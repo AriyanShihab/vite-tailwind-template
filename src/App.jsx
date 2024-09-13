fe4b03a5d8f7ceb1af3a5f1321e386735930efa4
Ariyan Shihab <devshihab111@gmail.com>
2024-09-13 21:21:53 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-800">
+      <div>
+        <h1 className="text-4xl text-slate-100">Hello Ariyan! </h1>
+        <p className="text-base text-cyan-200">witch topic do want to learn today?</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
